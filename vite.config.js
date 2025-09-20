d2e1dcd686e83127313be4c58127b9d5c25f9cdc
unknown <clear>
2025-09-20 22:26:09 +0530

    errro fix 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 2000, // large chunk warning kam karne ke liye
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'three', 'three-stdlib'], // Vite bundle split
+        },
+      },
+    },
+  },
 });
