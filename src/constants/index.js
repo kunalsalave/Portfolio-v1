f6c38bb11f7ce608a88514ee02783bcbf68efe8c
unknown <clear>
2025-09-20 20:55:50 +0530

    test

---
+++ src/constants/index.js
@@ -0,0 +1,344 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "E-commerce Platform",
+    description:
+      "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: ".Net",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 3,
+        name: "Ef Core",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Authentication & Authorization System",
+    description:
+      "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
+      "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+      "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+      "Connected to a secure SQLite database for user data storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/auth-system.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Auth0",
+        path: "/assets/logos/auth0.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Hotel Booking System",
+     description:
+      "A fully functional Hotel Booking web application built using MERN Stack (MongoDB, Express, React, Node.js).",
+    subDescription: [
+      "User authentication and management using Clerk.",
+      "Responsive UI with React and Tailwind CSS.",
+      "Book hotel rooms and receive email confirmations.",
+      "Secure online payments with Stripe.",
+      "Admin dashboard to manage rooms and bookings."
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/hotel-booking.png",
+    tags: [
+      {
+        id: 1,
+        name: "MongoDB",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Express",
+        path: "/assets/logos/dotnetcore.svg",
+      },
+      {
+        id: 3,
+        name: "React",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "Node.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 5,
+        name: "TailwindCss",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 6,
+        name: "Stripe",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+        
+    ],
+  },
+  {
+    id: 4,
+    title: "C++ Game Engine",
+    description:
+      "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Built a powerful rendering engine using OpenGL and C++.",
+      "Developed a physics engine with collision detection and particle effects.",
+      "Implemented a scripting system for easy game customization.",
+      "Optimized performance with multi-threading and efficient memory management.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "C++",
+        path: "/assets/logos/cplusplus.svg",
+      },
+      {
+        id: 2,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "Microsoft",
+        path: "/assets/logos/microsoft.svg",
+      },
+    ],
+  },
+{
+  id: 5,
+  title: "Zomato Clone (MERN Stack)",
+  description: "A full-stack Zomato clone with Reels-style video feed integration for enhanced user engagement.",
+  subDescription: [
+    "Developed complete backend using Node.js, Express, and MongoDB for restaurants, users, orders, and reviews management.",
+    "Implemented RESTful APIs for smooth frontend-backend communication.",
+    "Integrated Reels-style video feed on frontend using React for interactive user experience.",
+    "Handled authentication, authorization, and secure data management across the platform."
+  ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/FoodApp.png",
+    tags: [
+      {
+        id: 1,
+        name: "MongoDB",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "React.js",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "Node.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  
+  {
+    id: 6,
+    title: "Online Learning Platform",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built using Blazor WebAssembly for a seamless SPA experience.",
+      "Implemented video streaming with Azure Media Services.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/elearning.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Blazor",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Azure",
+        path: "/assets/logos/azure.svg",
+      },
+      {
+        id: 3,
+        name: "Stripe",
+        path: "/assets/logos/stripe.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ali-sanati/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+ {
+  title: "MERN Stack Intern",
+  job: "Self-Employed / Remote",
+  date: "2025-Present",
+  contents: [
+    "Working from home on full-stack projects using MongoDB, Express.js, React.js, and Node.js.",
+    "Developing responsive and interactive web applications with MERN Stack, implementing dynamic user interfaces.",
+    "Building and integrating RESTful APIs for smooth communication between frontend and backend.",
+    "Managing database operations, authentication, and CRUD functionality using MongoDB and Node.js.",
+    "Collaborating on real-world projects, enhancing coding standards, problem-solving skills, and version control practices."
+  ]
+},
+ {
+  title: "Back-End Developer / MERN Stack Developer",
+  job: "",
+  date: "2024-2025",
+  contents: [
+    "Developed complete backend using Node.js, Express, and MongoDB for Zomato Clone project.",
+    "Implemented RESTful APIs for restaurants, users, orders, and reviews management.",
+    "Integrated Reels-style video feed on frontend to enhance user engagement.",
+    "Handled authentication, authorization, and secure data management.",
+    "Collaborated with frontend to ensure seamless interaction between Node.js backend and React frontend."
+  ]
+},
+ {
+  title: "Freelance Developer",
+  job: "Self-Employed / MERN Stack Intern",
+  date: "2025-Present",
+  contents: [
+    "Developing full-stack web applications using MongoDB, Express.js, React, and Node.js (MERN).",
+    "Built a complete Zomato clone project with Node.js backend, React frontend, and Reels-style video feed integration.",
+    "Creating interactive portfolio and projects using Three.js, React, and modern web technologies to showcase technical expertise.",
+    "Continuously enhancing skills in modern web development, APIs, and responsive UI/UX design.",
+    "Managing work remotely, coordinating project development, and deploying applications online.",
+  ],
+}
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import Project from "../components/Project";
+import ProjectDetails from "../components/ProjectDetails"; // Import ProjectDetails
+import { myProjects } from "../constants";
+import { motion, useMotionValue, useSpring } from "motion/react";
+
+const Projects = () => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const springX = useSpring(x, { damping: 10, stiffness: 50 });
+  const springY = useSpring(y, { damping: 10, stiffness: 50 });
+
+  const handleMouseMove = (e) => {
+    x.set(e.clientX + 20);
+    y.set(e.clientY + 20);
+  };
+
+  const [preview, setPreview] = useState(null);
+
+  // ✅ Modal state
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  // Open modal
+  const openModal = (project) => {
+    setSelectedProject(project);
+  };
+
+  // Close modal
+  const closeModal = () => {
+    setSelectedProject(null);
+  };
+
+  return (
+    <section
+      onMouseMove={handleMouseMove}
+      className="relative c-space section-spacing"
+    >
+      <h2 className="text-heading">My Selected Projects</h2>
+      <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
+
+      {myProjects.map((project) => (
+        <Project
+          key={project.id}
+          {...project}
+          setPreview={setPreview}
+          openModal={openModal} // pass openModal to Project
+        />
+      ))}
+
+      {preview && (
+        <motion.img
+          className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80"
+          src={preview}
+          style={{ x: springX, y: springY }}
+        />
+      )}
+
+      {/* ✅ Modal */}
+      {selectedProject && (
+        <ProjectDetails {...selectedProject} closeModal={closeModal} />
+      )}
+    </section>
+  );
+};
+
+export default Projects;
